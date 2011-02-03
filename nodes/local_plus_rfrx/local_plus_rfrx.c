c39841e646dc9ffd4e9faac26fdc9343ca966831
Karl Palsson <github@tweak.net.au>
2011-02-03 00:39:18 +0000

    Just simply print the frequency to the serial port, to see if it aactually works.  (it does)

--- nodes/local_plus_rfrx/local_plus_rfrx.c
+++ nodes/local_plus_rfrx/local_plus_rfrx.c
@@ -92,9 +92,12 @@ USB_ClassInfo_CDC_Device_t VirtualSerial2_CDC_Interface =
 /** Main program entry point. This routine contains the overall program flow, including initial
  *  setup of all components and the main program loop.
  */
+static FILE USBSerialStream;
 int main(void)
 {
 	SetupHardware();
+        // Make a stream for channel 2, so we can write more debuggy stuff to it...
+        CDC_Device_CreateStream(&VirtualSerial2_CDC_Interface, &USBSerialStream);
 
 	sei();
 
@@ -148,8 +151,8 @@ int main(void)
                     packet.ksensors[0] = s1;
                     packet.ksensors[1] = s2;
                     packet.ksensors[2] = s3;
-
-                    CDC_Device_SendString(&VirtualSerial2_CDC_Interface, (char *)&packet, sizeof(kpacket));
+                    fprintf(&USBSerialStream, "freq = %u\r\n", frq);
+                    //CDC_Device_SendString(&VirtualSerial2_CDC_Interface, (char *)&packet, sizeof(kpacket));
                     frq = 0;
                 }
 
